dc4e88de24d968451437ff68d35899b6cf706a52
bitlischieber <r.lauber@gmx.ch>
2015-11-17 16:46:14 +0100

    Exercise 5 added

---
+++ Aufg5/hello_mod.c
@@ -0,0 +1,28 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+/* Metainformation
+** MODULE_LICENSE is mandatory.
+*/
+MODULE_AUTHOR("Bitlischieber");
+MODULE_DESCRIPTION("Hello world module");
+MODULE_LICENSE("GPL");
+
+/* Call after load of module.
+*/
+static int hello_init(void) {
+  printk(KERN_ALERT "Hello world\n");
+  return 0;
+}
+
+/* Call while unload module.
+*/
+static void hello_exit(void) {
+  printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+
+/* Initialissation
+** Makro defined methodes for load and unload module.
+*/
+module_init(hello_init);
+module_exit(hello_exit);
